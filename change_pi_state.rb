62198f038a46c1826c4b0941559687bae859123a
N M <nmusaelian@rallydev.com>
2014-04-30 14:51:00 -0600

    cleaned code

--- change_pi_state.rb
+++ change_pi_state.rb
@@ -3,7 +3,7 @@ require 'rally_api'
 
 #Setup custom app information
 headers = RallyAPI::CustomHttpHeader.new()
-headers.name = "change state of feature"
+headers.name = "My Utility: change state of feature"
 headers.vendor = "Nick M RallyLab"
 headers.version = "1.0"
 
@@ -11,22 +11,23 @@ headers.version = "1.0"
 config = {:base_url => "https://rally1.rallydev.com/slm"}
 config[:username] = "user@co.com"
 config[:password] = "secret"
-config[:workspace] = "W1"
-config[:project] = "P1"
-config[:headers] = headers #from RallyAPI::CustomHttpHeader.new()
+config[:workspace] = "W"
+config[:project] = "P"
+config[:headers] = headers 
 config[:version] = "v2.0"
 
 @rally = RallyAPI::RallyRestJson.new(config)
 
+workspace_ref = {"_ref" => "https://rally1.rallydev.com/slm/webservice/v2.0/workspace/1111" } 
+project_ref = {"_ref" => "https://rally1.rallydev.com/slm/webservice/v2.0/project/2222" } 
+
 queryState = RallyAPI::RallyQuery.new()
 queryState.type = :state
 queryState.fetch = "true"
-queryState.workspace = {"_ref" => "https://rally1.rallydev.com/slm/webservice/v2.0/workspace/1111" } 
-queryState.project = {"_ref" => "https://rally1.rallydev.com/slm/webservice/v2.0/project/2222" } #Company X
-queryState.page_size = 200 #optional - default is 200
-queryState.limit = 1000 #optional - default is 99999
+queryState.workspace = workspace_ref
+queryState.project = project_ref
 queryState.project_scope_up = false
-queryState.project_scope_down = true
+queryState.project_scope_down = false
 queryState.order = "Name Asc"
 queryState.query_string = "(TypeDef.Name = \"Feature\")"
 
@@ -36,19 +37,16 @@ state_results = @rally.find(queryState)
 
 state_results.each do |s|
 	s.read
-	#puts "Ref: #{s["_ref"]}, Name: #{s["Name"] }, TypeDef: #{s["TypeDef"]}" 
 	state_hash[s["Name"]] = s["_ref"]
 end	
 
 query = RallyAPI::RallyQuery.new()
 query.type = "portfolioitem/feature"
 query.fetch = "Name,FormattedID"
-query.workspace = {"_ref" => "https://rally1.rallydev.com/slm/webservice/v2.0/workspace/1111" } 
-query.project = {"_ref" => "https://rally1.rallydev.com/slm/webservice/v2.0/project/2222" } #Company X
-query.page_size = 200 #optional - default is 200
-query.limit = 1000 #optional - default is 99999
+query.workspace = workspace_ref 
+query.project = project_ref
 query.project_scope_up = false
-query.project_scope_down = true
+query.project_scope_down = false
 query.order = "Name Asc"
 query.query_string = "(Name = \"A Feature One\")"
 
@@ -69,7 +67,6 @@ end
 
 
 features.each do |f|
-	#field_updates = {"State" => "/state/12352608623"}
 	field_updates={"State" => state_hash["Developing"]}
 	f.update(field_updates)
 	puts "Feature #{f["FormattedID"]} is now in State: #{f["State"].to_s}"
