4719abad904cac30a72b0cbdcdb9411f447ef06c
N M <nmusaelian@rallydev.com>
2014-09-24 14:45:42 -0600

    first commit AttachmentsGT10000

---
+++ attachments/README.md
@@ -0,0 +1,19 @@
+get attachements where size > 10000
+=========================
+
+## Overview
+This script example is a proof of concept of how to get attachments in Rally and filter by size
+
+![](pic.png)
+
+The script was tested with 1.0.1 of rally_api gem
+
+This script is vailable AS IS. It is NOT supported by Rally.
+
+## License
+
+script is released under the MIT license.  See the file [LICENSE](./LICENSE) for the full text.
+
+##Documentation for SDK
+
+You can find the documentation on Rally toolkit for Ruby on this [site.](https://github.com/RallyTools/RallyRestToolkitForRuby)
---
+++ attachments/get_attachments_size_gt_10000.rb
@@ -0,0 +1,34 @@
+require 'rally_api'
+require 'date'
+
+
+#Setup custom app information
+headers = RallyAPI::CustomHttpHeader.new()
+headers.name = "My Utility"
+headers.vendor = "Nick M RallyLab"
+headers.version = "1.0"
+
+# Connection to Rally
+config = {:base_url => "https://rally1.rallydev.com/slm"}
+config[:api_key] = "_abc123"
+config[:workspace] = "W"
+config[:project] = "P"
+config[:headers] = headers #from RallyAPI::CustomHttpHeader.new()
+config[:version] = "v2.0"
+
+
+rally = RallyAPI::RallyRestJson.new(config)
+
+
+query = RallyAPI::RallyQuery.new()
+query.type = :attachment
+query.workspace = {"_ref" => "https://rally1.rallydev.com/slm/webservice/v2.0/workspace/12345" }
+query.fetch = "Artifact,LastUpdateDate,CreationDate,Size,User"
+query.query_string = "(Size > 10000)"
+
+results = rally.find(query)
+#get attachments above 10000
+results.each do |a|
+	puts "ref: #{a["_ref"]}, \n Size: #{a["Size"]}"
+	a.read
+end
