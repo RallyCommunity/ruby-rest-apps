9b78d02facc0815729d0af27a20c0c4bb88b9f36
Nick Musaelian <nmusaelian@nmusaellian-mbr.f4tech.com>
2015-01-27 11:00:52 -0700

    first commit get-milestones

---
+++ get_milestones_ex.rb
@@ -0,0 +1,28 @@
+require 'rally_api'
+
+
+#Setup custom app information
+headers = RallyAPI::CustomHttpHeader.new()
+headers.name = "get milestone data"
+headers.vendor = "Nick M RallyLab"
+headers.version = "1.0"
+
+# Connection to Rally
+config = {:base_url => "https://rally1.rallydev.com/slm"}
+config[:api_key] = "_abc123"
+config[:headers] = headers #from RallyAPI::CustomHttpHeader.new() 
+config[:version] = "v2.0"
+
+@rally = RallyAPI::RallyRestJson.new(config)
+
+
+#find changesets that meet criteria
+query = RallyAPI::RallyQuery.new()
+query.type = "milestone"
+query.fetch = "Name,TargetDate"
+query.query_string = "(TargetDate > 2015-01-01)"
+
+results = @rally.find(query)
+results.each do |m|
+  puts "Name: #{m["Name"]}, Target Date: #{m["TargetDate"]}"
+end
